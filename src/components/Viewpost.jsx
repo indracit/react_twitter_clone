cd7ec429100ebaee6245e90e35205d551ac05773
Indracit S <indrajithij802@gmail.com>
2023-07-14 14:46:42 +0530

    view post

--- src/components/Viewpost.jsx
+++ src/components/Viewpost.jsx
@@ -1,9 +1,198 @@
+import '../styles/sidebar.scss';
+
+const userDetails = {
+id: 1,
+name:'Indrajith',
+username:'indraji746463',
+isVerified : true,
+}
+
+const post = [
+  {
+    id:1,
+    body : 'In every day, there are 1,440 minutes. That means we have 1,440 daily opportunities to make a positive impact.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'2h'
+  },
+  {
+    id:2,
+    body : 'You are never too old to set another goal or to dream a new dream.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'4h'
+  },
+  {
+    id:3,
+    body : 'Optimism is the faith that leads to achievement. Nothing can be done without hope and confidence.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'5h'
+  },
+  {
+    id:4,
+    body : 'It always seems impossible until its done.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'6h'
+  },
+  {
+    id:5,
+    body : 'It does not matter how slowly you go as long as you do not stop.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'7h'
+  },
+  {
+    id:6,
+    body : 'If youre going through hell, keep going.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'8h'
+  },
+  {
+    id:7,
+    body : 'It does not matter how slowly you go as long as you do not stop.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'9h'
+  },
+  {
+    id:8,
+    body : 'If youre going through hell, keep going.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'10h'
+  },
+  {
+    id:9,
+    body : 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'11h'
+  },
+  {
+    id:10,
+    body : 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'12h'
+  },
+  {
+    id:11,
+    body : 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'13h'
+  },
+  {
+    id:12,
+    body : 'Start where you are. Use what you have. Do what you can.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'14h'
+  },
+  {
+    id:13,
+    body : 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'15h'
+  },
+  {
+    id:14,
+    body : 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'16h'
+  },
+  {
+    id:15,
+    body : 'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'17h'
+  },
+  {
+    id:16,
+    body : 'Believe in yourself! Have faith in your abilities! Without a humble but reasonable confidence in your own powers you cannot be successful or happy.',
+    otherDetails : {
+      comments: 200,
+      retweets : 100,
+      likes : 400,
+      views : 288,
+    },
+    postedtime:'18h'
+  },
+]
 
 
 const Viewpost = () => {
   return (
-    <div>Viewpost</div>
-  )
+    <div>
+      <div className="userimg">I</div>
+    </div>
+  )  
 }
 
 export default Viewpost
